0907a8a10f86cc2921d06a860f5f434577ea115e
Kentaro <zerg.dd823@gmail.com>
2020-05-26 10:07:09 +0900

    [fix]Remove unusing contents.

--- src/pages/js/main.js
+++ src/pages/js/main.js
@@ -1,16 +1,32 @@
 var ros = new ROSLIB.Ros({ url : 'ws://' + location.hostname + ':9000' });
-ros.on('connection', function() {console.log('websocket: connected'):});
+ros.on('connection', function() {console.log('websocket: connected');});
 ros.on('error', function(error) {console.log('websocket error: ', error);});
 ros.on('close', function() {console.log('websocket: closed');});
-ya
-var ls = new ROSLIB.Topic({
-	ros: ros,
-	name: '/twice',
-	messageType: 'std_msge/Int32'
-});
 
-ls.subscribe(function(message){
-	str = JSON.stringify(message);
-	document.getElementById("count").innerHTML = str;
-	console.log(str);
-});
+var ls2 = new ROSLIB.Topic({
+	ros: ros,
+	name: '/detect',
+	messageType: 'std_msgs/Int32'
+})
+
+function detect_on(){
+	flagstr = ''
+	for(i=0; i<document.chbox.elements.length ; i++){
+		appendstr = '0';
+		if(document.chbox.elements[i].checked){
+			appendstr = '1';
+		}
+		flagstr += appendstr;
+	}
+	
+	console.log('bin: ' + flagstr , 'int: ' + parseInt(flagstr, 2))
+
+	ls2.publish({ data: parseInt(flagstr, 2) })
+}
+
+function all_off(){
+	for(i=0; i<document.chbox.elements.length ; i++){
+        	document.chbox.elements[i].checked = false;
+        }
+	ls2.publish({data: 0});
+}
